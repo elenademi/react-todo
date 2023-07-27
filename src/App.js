642ca8bcf507d20f40ba8edea1ad0a02690c59c7
elenademi <118300498+elenademi@users.noreply.github.com>
2023-07-27 02:28:32 -0400

    asynchronous data fetching, conditional rendering

--- src/App.js
+++ src/App.js
@@ -2,46 +2,65 @@ import  React, { useState }  from 'react';
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
 
-function useSemiPersistentState() {
-  
-  const [todoList, setTodoList]= useState(JSON.parse(localStorage.getItem('savedTodoList')||[]))
 
-  React.useEffect(function() {localStorage.setItem('savedTodoList', JSON.stringify(todoList));},[todoList])
-    
-  
-  
-  return([todoList, setTodoList]
-
-
-)}
 
 function App() {
+
   
-  // const [todoList, setTodoList]= useState(JSON.parse(localStorage.getItem('savedTodoList')))
+  const [todoList, setTodoList]=useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-  const [todoList, setTodoList]= useSemiPersistentState()
+  React.useEffect(() => {
+    const getData = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({ data: { todoList: JSON.parse(localStorage.getItem('savedTodoList')||[]) } });
+      }, 2000);
+    });
 
-  // React.useEffect(function() {localStorage.setItem('savedTodoList', JSON.stringify(todoList));},[todoList])
+    getData.then((result) => {
+      setTodoList(result.data.todoList);
+      setIsLoading(false); 
+    });
+  }, []);
+
+   React.useEffect(function() { 
+
+      if(!isLoading) { 
+
+    localStorage.setItem('savedTodoList', JSON.stringify(todoList));}},[todoList])
+
+   
+  
     
 
   function addTodo (newTodo) {
     setTodoList([...todoList, newTodo])
   }
 
+
   function removeTodo(id) {
     const newTodoList = todoList.filter((item) => item.id !==id)
       setTodoList(newTodoList)
       
   }
+
   
   return (
      <>
      
      <h1 style={{ textAlign: 'center' }}>Todo List</h1>
+
      <AddTodoForm onAddTodo={addTodo}/>
-     <TodoList todoList={todoList} onRemoveTodo={removeTodo}/>
+
+     {isLoading ? (
+       <p>Loading ...</p>
+
+          ) : (
      
-     {/* <p> <strong>{newTodo}</strong></p> */}
+      <TodoList todoList={todoList} onRemoveTodo={removeTodo}/>)}
+
+     
+     
      
           
       </>
@@ -49,4 +68,4 @@ function App() {
 
 }
 
-export default App;
+export default App
