8b11d9d37c48cdc324d0cc09b3cf27cfde8c7854
elenademi <118300498+elenademi@users.noreply.github.com>
2023-08-08 10:15:05 -0400

    Update TodoList.js

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,7 +2,6 @@ import React from 'react';
 import TodoListItem from './TodoListItem';
 
 {/*const todoList = [
-
     {
       title: "Complete Assignment",
        id: 1,
@@ -26,7 +25,6 @@ function TodoList({todoList,onRemoveTodo}) {
   
         return(
         <ul>
-
             {/* { props.todoList.map((item) => <TodoListItem key={item.id} todo={item}/>)}  */}
             {todoList.map(function(todo) {
               return <TodoListItem  key={todo.id} todo={todo} onRemoveTodo={onRemoveTodo} />
@@ -34,7 +32,6 @@ function TodoList({todoList,onRemoveTodo}) {
               })}
            
         </ul>
-
     );
 };
 
@@ -44,3 +41,8 @@ function TodoList({todoList,onRemoveTodo}) {
 
 
 export default TodoList
+
+  
+  
+  
+
